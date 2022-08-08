025d311106664985a94c3fd9368a22e48b2a983b
Ruslan Lyga <rlphila@gmail.com>
2022-08-07 23:36:19 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          
+          Expenses Project
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
